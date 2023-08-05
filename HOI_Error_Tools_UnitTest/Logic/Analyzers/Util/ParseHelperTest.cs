36787dd8f61417a8547fe0f46192af8f8f73ba88
millennium <1543255825@qq.com>
2023-08-05 13:30:35 +0800

    添加需要的部分测试文件

---
+++ HOI_Error_Tools_UnitTest/Logic/Analyzers/Util/ParseHelperTest.cs
@@ -0,0 +1,20 @@
+﻿using HOI_Error_Tools.Logic.Analyzers.Util;
+
+namespace HOI_Error_Tools_UnitTest.Logic.Analyzers.Util;
+
+[TestFixture]
+[TestOf(typeof(ParseHelper))]
+public class ParseHelperTest
+{
+    [Test]
+    public void GetAllLeafKeyAndValueInAllNode()
+    {
+        var filePath = Path.Combine(Environment.CurrentDirectory, "TestText", "ParseHelperTestText.txt");
+        var parser = new HOI_Error_Tools.Logic.HOIParser.CWToolsParser(filePath);
+        var rootNode = parser.GetResult();
+
+        var result = ParseHelper.GetAllLeafKeyAndValueInAllNode(rootNode, "bbb");
+
+    }
+    
+}
