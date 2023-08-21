8c9fc7542169364b0ca6e56ed65416c22b551dca
millennium <1543255825@qq.com>
2023-08-21 11:02:51 +0800

    删除多余 XML Doc

--- HOI_Error_Tools/View/SettingsWindowView.xaml.cs
+++ HOI_Error_Tools/View/SettingsWindowView.xaml.cs
@@ -2,9 +2,6 @@
 
 namespace HOI_Error_Tools.View;
 
-/// <summary>
-/// SettingsWindowView.xaml 的交互逻辑
-/// </summary>
 public partial class SettingsWindowView : Window
 {
     public SettingsWindowView()
