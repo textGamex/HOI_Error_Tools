d3f087f362a0e1240b8db37124c99e29346a31c6
textGamex <textGamex@outlook.com>
2023-09-25 18:23:30 +0800

    add: 实现通过 vscode 打开错误文件

--- HOI_Error_Tools/ViewModels/ErrorFileInfoViewModel.cs
+++ HOI_Error_Tools/ViewModels/ErrorFileInfoViewModel.cs
@@ -1,8 +1,12 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.Input;
 using HOI_Error_Tools.Services;
+using Microsoft.Extensions.DependencyInjection;
+using NLog;
 
 namespace HOI_Error_Tools.ViewModels;
 
@@ -11,8 +15,12 @@ public partial class ErrorFileInfoViewModel : ObservableObject
     [ObservableProperty]
     private List<FileInfoVO> _data;
 
-    public ErrorFileInfoViewModel(IErrorFileInfoService errorFileInfoService)
+    private readonly IMessageBox _messageBox;
+    private static readonly ILogger Log = App.Current.Services.GetRequiredService<ILogger>();
+
+    public ErrorFileInfoViewModel(IErrorFileInfoService errorFileInfoService, IMessageBox messageBox)
     {
+        _messageBox = messageBox;
         var errorInfoList = errorFileInfoService.GetFileErrorInfoList();
         _data = new List<FileInfoVO>(errorInfoList.Count);
         foreach (var fileInfo in errorInfoList)
@@ -25,6 +33,28 @@ public partial class ErrorFileInfoViewModel : ObservableObject
     private static void OpenFolder(FileInfoVO fileInfo)
     {
         _ = Process.Start("Explorer.exe", $"/select, {fileInfo.FilePath}");
+    }
+
+    [RelayCommand]
+    private void OpenFileInVsCode(FileInfoVO fileInfo)
+    {
+        try
+        {
+            // see https://code.visualstudio.com/docs/editor/command-line
+            var info = new ProcessStartInfo()
+            {
+                FileName = "code",
+                Arguments = $"-g \"{fileInfo.FilePath}:{fileInfo.ErrorLine}:0\"",
+                UseShellExecute = true,
+            };
+            var process = Process.Start(info);
+            process?.Close();
+        }
+        catch (Exception e)
+        {
+            _messageBox.ErrorTip("VS Code 启动失败, 请检查您的电脑是否安装了 VS Code");
+            Log.Warn(e, "VS Code 启动失败");
+        }
     }
 
     public record FileInfoVO(string FilePath, long ErrorLine);
